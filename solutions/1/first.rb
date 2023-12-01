bbde2817013589a8b691dedfdad7d021099e736d
careyi3 <ian@iancarey.ie>
2023-12-01 10:38:11 +0000

    Day 1

--- solutions/1/first.rb
+++ solutions/1/first.rb
@@ -3,7 +3,15 @@
 module Day1
   module Part1
     def self.run(path, _)
-
+      nums = []
+      FileReader.for_each_line(path) do |line|
+        line_nums = []
+        line.chars.each do |char|
+          line_nums << char if char.to_i.to_s == char
+        end
+        nums << (line_nums.first + line_nums.last).to_i
+      end
+      puts nums.sum
     end
   end
 end
--- solutions/1/second.rb
+++ solutions/1/second.rb
@@ -3,7 +3,34 @@
 module Day1
   module Part2
     def self.run(path, _)
-
+      map = {
+        'one' => 1,
+        '1' => 1,
+        'two' => 2,
+        '2' => 2,
+        'three' => 3,
+        '3' => 3,
+        'four' => 4,
+        '4' => 4,
+        'five' => 5,
+        '5' => 5,
+        'six' => 6,
+        '6' => 6,
+        'seven' => 7,
+        '7' => 7,
+        'eight' => 8,
+        '8' => 8,
+        'nine' => 9,
+        '9' => 9
+      }
+      nums = []
+      FileReader.for_each_line(path) do |line|
+        word_nums = line.scan(Regexp.new("(?=(#{map.keys.join('|')}))")).flatten
+        num = (map[word_nums.first].to_s + map[word_nums.last].to_s).to_i
+        nums << num
+        # puts "#{word_nums.join(',')} = #{num}"
+      end
+      puts nums.sum
     end
   end
 end
